380d9298995521c924d346a0bcf3fd63287d1a71
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2021-01-20 17:15:18 +0800

    Update BIT-乐学-消除姓名中空格.user.js

--- TamperMonkey/BIT-乐学-消除姓名中空格.user.js
+++ TamperMonkey/BIT-乐学-消除姓名中空格.user.js
@@ -1,18 +1,19 @@
 // ==UserScript==
 // @name         BIT-乐学-消除姓名中空格
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  消除中文姓名中间的空格
 // @author       Y.D.X.
 // @match        http://lexue.bit.edu.cn/mod/forum/discuss.php*
-// @match        http://lexue.bit.edu.cn*
+// @match        http://lexue.bit.edu.cn/mod/forum/view.php*
+// @match        http://lexue.bit.edu.cn/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    document.querySelectorAll("a[href*='/user/view.php'], .usertext, .fullname").forEach(e => {
-        console.log(e);
+    console.log("!!!");
+    document.querySelectorAll("a[href*='/user/view.php'], .author-info > .text-truncate, .usertext, .fullname").forEach(e => {
         if(!e.textContent.match(/[a-zA-Z]/)){
             e.textContent = e.textContent.replaceAll(" ", "");
         }
