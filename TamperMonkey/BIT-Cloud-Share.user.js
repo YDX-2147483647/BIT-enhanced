f8108f6220e1c841c848cf1e2d125ba0e4214268
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2021-02-03 19:16:40 +0800

    Update URL for WebVPN

--- TamperMonkey/BIT-Cloud-Share.user.js
+++ TamperMonkey/BIT-Cloud-Share.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         BIT-Cloud-Share
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  enable the share function.
 // @author       Y.D.X.
 // @match        http://pan.bit.edu.cn/disk
+// @match        https://webvpn.bit.edu.cn/http/77726476706e69737468656265737421e0f64fd225397c1e7b0c9ce29b5b/disk
 // @grant        none
 // ==/UserScript==
 
