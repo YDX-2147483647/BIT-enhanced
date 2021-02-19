1f60506ce29ed61d3986123b38e432a0c50e93e2
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2021-02-19 12:13:44 +0800

    增加适用范围，另外整理了格式

--- TamperMonkey/BIT-乐学-消除姓名中空格.user.js
+++ TamperMonkey/BIT-乐学-消除姓名中空格.user.js
@@ -1,18 +1,26 @@
 // ==UserScript==
 // @name         BIT-乐学-消除姓名中空格
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  消除中文姓名中间的空格
 // @author       Y.D.X.
 // @match        http://lexue.bit.edu.cn/mod/forum/discuss.php*
 // @match        http://lexue.bit.edu.cn/mod/forum/view.php*
+// @match        http://lexue.bit.edu.cn/user/profile.php*
 // @match        http://lexue.bit.edu.cn/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    document.querySelectorAll("a[href*='/user/view.php'], .author-info > .text-truncate, .usertext, .fullname").forEach(e => {
+    let selectors = [
+        "a[href*='/user/view.php']", // 通用
+        ".usertext", // 通用：header 中头像的左边
+        ".fullname", // 首页-已登录用户
+        "author-info > .text-truncate", // forum/view.php
+        "#page-header h1, head title" // user/profile.php
+    ];
+    document.querySelectorAll(selectors.join(", ")).forEach(e => {
         if(!e.textContent.match(/[a-zA-Z]/)){
             e.textContent = e.textContent.replaceAll(" ", "");
         }
