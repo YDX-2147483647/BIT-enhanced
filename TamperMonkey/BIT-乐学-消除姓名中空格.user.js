8c1d6376144f5b15c4579c889cb47253fb26fe1d
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2021-02-28 18:59:38 +0800

    发现消息菜单里的小组参与者不会处理，但不打算处理了

--- TamperMonkey/BIT-乐学-消除姓名中空格.user.js
+++ TamperMonkey/BIT-乐学-消除姓名中空格.user.js
@@ -19,6 +19,7 @@
         ".fullname", // 首页-已登录用户
         "author-info > .text-truncate", // forum/view.php
         "#page-header h1, head title" // user/profile.php
+        // "[data-region='members-list-container'] [data-route='view-conversation'] > h6.ml-2" // 通用：消息菜单-小组-参与者；动态且不常用，不管了。
     ];
     document.querySelectorAll(selectors.join(", ")).forEach(e => {
         if(!e.textContent.match(/[a-zA-Z]/)){
