d8bf9d0349264d465de676eba65f7b0e7ff67e30
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2021-04-10 22:29:55 +0800

    新增总体和用户报表

--- TamperMonkey/BIT-乐学-消除姓名中空格.user.js
+++ TamperMonkey/BIT-乐学-消除姓名中空格.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BIT-乐学-消除姓名中空格
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  消除中文姓名中间的空格
 // @author       Y.D.X.
 // @match        http://lexue.bit.edu.cn/mod/forum/discuss.php*
@@ -9,6 +9,7 @@
 // @match        http://lexue.bit.edu.cn/user/profile.php*
 // @match        http://lexue.bit.edu.cn/user/view.php*
 // @match        http://lexue.bit.edu.cn/mod/assign/view.php*
+// @match        http://lexue.bit.edu.cn/grade/report/*
 // @match        http://lexue.bit.edu.cn/*
 // @match        https://webvpn.bit.edu.cn/http/77726476706e69737468656265737421fcf25989227e6a596a468ca88d1b203b/*
 // @grant        none
@@ -17,7 +18,7 @@
 (function () {
     'use strict';
     let selectors = [
-        "a[href*='/user/view.php']", // 通用
+        "a[href*='/user/view.php'], #page-navbar a[href*='/user/profile.php']", // 通用
         ".usertext", // 通用：header 中头像的左边
         ".fullname", // 首页-已登录用户
         "author-info > .text-truncate", // forum/view.php
@@ -31,6 +32,12 @@
             element.textContent = element.textContent.replaceAll(" ", "");
         }
     }
+    function trim_name_string(name) {
+        if (!/[a-zA-Z]/.test(name))
+            return name.replaceAll(" ", "");
+        else
+            return name;
+    }
 
     // assign/view.php
     // 整理评分人名字这一格的格式。
@@ -39,6 +46,16 @@
         let user_url = cell_gradedBy.querySelector("a").href;
         let user_name = cell_gradedBy.textContent;
         cell_gradedBy.innerHTML = `<a href="${user_url}">${user_name}</a >`;
+    }
+
+    // grade/report/(overview|user)/index.php
+    if (/^\/grade\/report\/(overview|user)\/index\.php$/.test(window.location.pathname)) {
+        let headline = document.querySelector("#page-content  #region-main > [role='main'] > #maincontent + h2");
+        if (headline) {
+            headline.textContent = headline.textContent.replace(/^(.{2}报表) - (.+)$/, (match, prefix, name) => {
+                return [prefix, trim_name_string(name)].join(' - ');
+            });
+        }
     }
 
     document.querySelectorAll(selectors.join(", ")).forEach(trim_name);
