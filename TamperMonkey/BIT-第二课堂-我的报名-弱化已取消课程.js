0eb1ca2708538b6df6b2787fad7eca9e29f64d8a
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2021-05-10 00:30:25 +0800

    Create BIT-第二课堂-我的报名-弱化已取消课程.js

---
+++ TamperMonkey/BIT-第二课堂-我的报名-弱化已取消课程.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         BIT-第二课堂-我的报名-弱化已取消课程
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  给可报名的课程设置颜色
+// @author       Y.D.X.
+// @match        http://dekt.bit.edu.cn/course/QueryMyCourseList.jsp*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    function add_style_sheet() {
+        let sheet = document.createElement('style');
+        sheet.innerHTML = `
+        #main > form[name='form1'] tr.cancelled > td,
+        #main > form[name='form1'] tr.cancelled > td > a {
+            color: lightgray;
+        }
+        `;
+        document.head.appendChild(sheet);
+    }
+
+    function check() {
+        for (let row of document.querySelectorAll("#main > form[name='form1'] table > tbody > tr:nth-child(n+2)")) {
+            row.children[7].style.paddingLeft = '0'; // 原来是 5px，会让“开放浏览”分到两行
+
+            let cancel_description = row.children[8].textContent.trim();
+            if (cancel_description == '取消原因')
+                row.classList.add('cancelled');
+        }
+    }
+    
+    add_style_sheet();
+    check();
+})();
