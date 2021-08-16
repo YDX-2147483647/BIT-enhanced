00e8558f1773e97db15794f0056face462fb202b
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2021-08-16 20:26:41 +0800

    Create BIT-WebVPN-删除冗余.js
    之前漏掉了啊。

---
+++ TamperMonkey/BIT-WebVPN-删除冗余.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         BIT-WebVPN-删除冗余
+// @namespace    http://tampermonkey.net/
+// @version      0.2.1
+// @description  删除学校名称，简化过长的名字，删除名字里重复的地址
+// @author       Y.D.X.
+// @match        https://webvpn.bit.edu.cn/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // 如果修改了“我的收藏”，会重新刷新，但脚本不会重新运行，因而失效。
+    // 不过这种事件不常发生，所以就不考虑了……
+    document.querySelectorAll(".vpn-content div.vpn-content-block-panel__content > p:nth-child(1), #vpn-bastion-list .bastion-item > .bastion-item__content > .bastion-item__title").forEach(e => {
+        e.textContent = e.textContent.replace("北京理工大学", "").replace(/系统$/, "");
+        let address = e.parentNode.lastElementChild.textContent;
+        let redundant_address = e.textContent.match(/[0-9a-zA-Z.]+/);
+        if(redundant_address){
+            if(address.includes(redundant_address[0]) && e.textContent != redundant_address[0]){
+                e.textContent = e.textContent.replace(redundant_address[0], "");
+            }
+        }
+        e.textContent = e.textContent.replace(/^[_-]/, "").replace(/[_-]$/, "");
+    });
+})();
