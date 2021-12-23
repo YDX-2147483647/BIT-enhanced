28ea042fc097c775016fcfd932886085609141a2
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2021-12-23 21:33:21 +0800

    fix 修改了两个 CSS 选择器，现在不会再误把旁边“活动”栏的“编程练习”或“测验”当成题了

--- TamperMonkey/BIT-乐学-编程题和测验状态.js
+++ TamperMonkey/BIT-乐学-编程题和测验状态.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BIT-乐学-编程题和测验状态
 // @namespace    http://tampermonkey.net/
-// @version      0.2.1
+// @version      0.2.2
 // @description  读取编程题状态并显示
 // @author       Y.D.X.
 // @match        https://lexue.bit.edu.cn/course/view.php*
@@ -11,10 +11,10 @@
 // 上一版本：BIT-Programming-编程题状态（0.1）。
 
 (function () {
-    'use strict';
+    'use strict'
 
     function add_style_sheet() {
-        let sheet = document.createElement('style');
+        const sheet = document.createElement('style')
         sheet.innerHTML = `
         .problem-state-true::after,
         .quiz-state-true::after
@@ -50,35 +50,35 @@
             content: "（未）";
             color: skyblue;
         }
-        `;
-        document.head.appendChild(sheet);
+        `
+        document.head.appendChild(sheet)
     }
 
     async function check_all_problems() {
-        const problems = document.querySelectorAll("a[href*='lexue.bit.edu.cn/mod/programming/']");
+        const problems = document.querySelectorAll("a[href*='lexue.bit.edu.cn/mod/programming/view']")
         for (const p of problems) {
-            let state = await get_problem_state(get_URL_id(p.href));
-            p.classList.add("problem-state-" + String(state));
+            const state = await get_problem_state(get_URL_id(p.href))
+            p.classList.add("problem-state-" + String(state))
         }
     }
     async function check_all_quizzes() {
-        const quizzes = document.querySelectorAll("a[href*='lexue.bit.edu.cn/mod/quiz/']");
+        const quizzes = document.querySelectorAll("a[href*='lexue.bit.edu.cn/mod/quiz/view']")
         for (const q of quizzes) {
-            let state = await get_quiz_state(q.href);
-            q.classList.add('quiz-state-' + String(state));
+            const state = await get_quiz_state(q.href)
+            q.classList.add('quiz-state-' + String(state))
         }
     }
 
 
     function get_URL_id(href) {
-        let params = (new URL(href)).searchParams;
-        return params.get('id');
+        const params = (new URL(href)).searchParams
+        return params.get('id')
     }
     function get_problem_result_URL(problem_id) {
-        return document.location.origin + "/mod/programming/result.php?" + `id=${problem_id}`;
+        return document.location.origin + "/mod/programming/result.php?" + `id=${problem_id}`
     }
 
-    const parser = new DOMParser();
+    const parser = new DOMParser()
 
     /**
      * 获取编程题的情况
@@ -89,23 +89,23 @@
      * @returns false 已提交但尚未全部通过
      */
     async function get_problem_state(problem_id) {
-        const response = await fetch(get_problem_result_URL(problem_id));
-        const response_text = await response.text();
-        const doc = parser.parseFromString(response_text, 'text/html');
+        const response = await fetch(get_problem_result_URL(problem_id))
+        const response_text = await response.text()
+        const doc = parser.parseFromString(response_text, 'text/html')
 
-        const headings = doc.querySelectorAll("[role=main] > h3");
+        const headings = doc.querySelectorAll("[role=main] > h3")
         if (headings[0].textContent == "查看程序的测试结果" &&
             headings[0].nextSibling.textContent == "找不到您的程序")
-            return null;
+            return null
         else if (headings[headings.length - 1].textContent != "测试结果")
-            return undefined;
+            return undefined
         else {
-            const result_text = doc.querySelector('#test-result-detail > p:first-child').textContent;
-            let result = result_text.match(/测试结果：共 (?<total>\d+) 个测试用例，您的程序通过了其中的 (?<accepted>\d+) 个，未能通过的有 (?<rejected>\d+) 个。/).groups;
+            const result_text = doc.querySelector('#test-result-detail > p:first-child').textContent
+            let result = result_text.match(/测试结果：共 (?<total>\d+) 个测试用例，您的程序通过了其中的 (?<accepted>\d+) 个，未能通过的有 (?<rejected>\d+) 个。/).groups
             for (const i in result)
-                result[i] = parseInt(result[i]);
+                result[i] = parseInt(result[i])
 
-            return result.rejected == 0;
+            return result.rejected == 0
         }
     }
 
@@ -116,17 +116,17 @@
      * @returns false 未完成
      */
     async function get_quiz_state(href) {
-        let response = await fetch(href);
-        const response_text = await response.text();
-        const doc = parser.parseFromString(response_text, 'text/html');
+        const response = await fetch(href)
+        const response_text = await response.text()
+        const doc = parser.parseFromString(response_text, 'text/html')
 
-        const heading = doc.querySelector("[role=main] > h3");
-        return heading != null && heading.textContent == "您上次尝试的概要";
+        const heading = doc.querySelector("[role=main] > h3")
+        return heading != null && heading.textContent == "您上次尝试的概要"
     }
 
 
-    add_style_sheet();
-    check_all_problems();
-    check_all_quizzes();
+    add_style_sheet()
+    check_all_problems()
+    check_all_quizzes()
 
-})();
+})()
