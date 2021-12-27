570105f3984162495e23c20ffe2f5cf823e861bb
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2021-12-27 13:05:08 +0800

    中国大学MOOC-修复图片

---
+++ TamperMonkey/中国大学MOOC-修复图片.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         中国大学MOOC-修复图片
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0
+// @description  修复讨论区的图片
+// @author       Y.D.X.
+// @require      https://gitee.com/YDX-2147483647/BIT-enhanced/raw/main/TamperMonkey/lib/mooc.js
+// @match        https://www.icourse163.org/learn*
+// @match        https://www.icourse163.org/spoc/learn*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    "use strict"
+
+    function change_origin(url) {
+        return 'https://img-ph-mirror.nosdn.127.net' + (new URL(url)).pathname
+    }
+
+    function main() {
+        document.querySelectorAll("img[src^='https://img0.ph.126.net']")
+            .forEach(img => { img.src = change_origin(img.src) })
+    }
+
+    Mooc.on_every_loaded(main)
+    window.addEventListener('hashchange', () => Mooc.on_every_loaded(main))
+
+})()
