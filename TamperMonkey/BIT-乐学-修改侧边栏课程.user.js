00f8793e6fdce8db2e8e2defb06bf28b251f8c18
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2022-06-19 13:53:55 +0800

    “BIT-乐学-修改侧边栏课程”的设置改用“存储”

--- TamperMonkey/BIT-乐学-修改侧边栏课程.user.js
+++ TamperMonkey/BIT-乐学-修改侧边栏课程.user.js
@@ -1,18 +1,37 @@
 // ==UserScript==
 // @name         BIT-乐学-修改侧边栏课程
 // @namespace    http://tampermonkey.net/
-// @version      1.2.1
+// @version      1.3.0-beta
 // @description  在侧边栏添加并重新排列课程
 // @author       Y.D.X.
 // @match        https://lexue.bit.edu.cn/*
 // @match        https://webvpn.bit.edu.cn/https/77726476706e69737468656265737421fcf25989227e6a596a468ca88d1b203b/*
-// @grant        none
+// @grant        GM.getValue
 // ==/UserScript==
 
 // Moodle 好像是按某种规则挑选出最多 10 个课程。然而它并不能完全猜中，还不时更新，导致每次更新都要改。
 // 在个人主页的课程概览可以筛选“进行中”“尚未开始的”或“过去的”课程。
 
-(function () {
+// 您需要自己配置哪些课程要隐藏，哪些要显示。
+// 1. 改为高级配置模式：前往 TamperMonkey → 设置 → 通用 → 配置模式，选为“高级”。
+// 2. 找到“存储”：到这个脚本的编辑页，在上边栏切换到“存储”（位置与“编辑器”“设置”并排）。
+// 3. 写入配置：在“存储”一栏写入 JSON，一般照猫画虎即可。
+//    例如——
+//    {
+//        "show": [
+//            { "name": "信号与系统-实验", "id": "10664", "icon": "signal" },
+//            { "name": "电磁场与电磁波", "id": "10713", "icon": "globe" }
+//        ],
+//        "hide": [
+//            { "name": "物理实验B-绪论", "id": "7661", "icon": "flask" },
+//            "更多",
+//            { "name": "学术用途英语12021-2022-1许子艳", "id": "8751", "comment": "错误课程" },
+//            "乐学使用简明教程"
+//        ]
+//    }
+//    详情见下面注释里的`Config`及`CourseDescription`。
+
+(async function () {
     'use strict'
 
     /**
@@ -24,55 +43,26 @@
      * @property {?string} comment - 给自己看的注释，比如为什么要屏蔽这门课
      */
 
-    const config = {
-        /**
-         * 想添加的课程，有序，显示时前面的在上
-         * @type {CourseDescription[]}
-         */
-        show: [
-            { name: '信号与系统-实验', id: '10664', icon: 'signal' },
-            { name: '电磁场与电磁波', id: '10713', icon: 'globe' },
-        ],
-        /**
-         * 想隐藏的课程
-         * @type {(string | CourseDescription)[]}
-         */
-        hide: [
-            { name: "毛泽东思想", id: "9771", icon: "user" },
-            { name: '数字电路-期末考试', id: '11037', icon: 'bolt' },
-            { name: "集合论", id: "9628", icon: "plus-circle" },
-            { name: '半导体物理', id: '10760', icon: 'battery-full' },
-            { name: '电磁场与电磁波-实验', id: '10729', icon: 'rss' },
-            { name: '数字电路-实验', id: '10635', icon: 'bolt' },
-            { name: "网络开拓者", id: "877", icon: "star-o" },
-            { name: 'Java', id: '8989'},
-            { name: "模拟电子学-硬件实验", id: "8819", icon: "bolt" },
-            { name: "物理 2", id: "8853", icon: "globe" },
-            { name: "模拟电子学-软件实验", id: "9250", icon: "television" },
-            { name: '2021网络安全宣传周', id: '1727' },
-            { name: "ECE实习", id: "8369", icon: "code" },
-            { name: '学术用途英语12021-2022-1许子艳', id: '8751', comment: '错误课程' },
-            { name: "工程制图习题", id: "1038" },
-            { name: "MCM 2021春", id: "6693", icon: "line-chart" },
-            { name: "数据结构", id: "6556", icon: "database" },
-            { name: "唐宋诗词欣赏", id: "7154", icon: "paragraph" },
-            "2020网络安全宣传周",
-            { name: "CUMCM", id: "4246", icon: "line-chart" },
-            { name: "物理实验B-绪论", id: "7661", icon: "flask" },
-            "更多",
-            { name: "工科数学分析 2", id: "6712", icon: "superscript" },
-            { name: "线性代数A", id: "6721", icon: "square-o" },
-            { name: "物理A 1", id: "7348", icon: "globe" },
-            { name: "中国近现代史", id: "7880", icon: "book" },
-            { name: "工训", id: "7297", icon: "wrench" },
-            "乐学使用简明教程",
-            "C语言程序设计（2020级电子信息-中文班）期末考试",
-            "习近平新时代中国特色社会主义思想概论",
-            "思想道德修养与法律基础",
-            "信息技术基础",
-        ]
+    /**
+     * GM 存储的配置
+     * @typedef {Object} Config
+     * @property {CourseDescription[]} show - 想添加的课程，有序，显示时前面的在上
+     * @property {(string | CourseDescription)[]} hide - 想隐藏的课程
+     */
+
+
+    /**
+     * 加载 GM 存储的配置
+     * @returns {Promise<Config>}
+     */
+    async function load_config() {
+        return {
+            show: await GM.getValue('show', []),
+            hide: await GM.getValue('hide', []),
+        }
     }
 
+    const config = await load_config()
 
     /**
      * `test`是否与`ref`是同一课程
