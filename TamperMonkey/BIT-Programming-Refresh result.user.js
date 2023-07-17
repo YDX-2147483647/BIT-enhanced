9c515a8c10819e8f005451cefb6521e6734580a2
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2023-07-17 11:16:32 +0800

    style: No implicit global, new line on `if`

--- TamperMonkey/BIT-Programming-Refresh result.user.js
+++ TamperMonkey/BIT-Programming-Refresh result.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BIT-Programming-Refresh result
 // @namespace    http://tampermonkey.net/
-// @version      0.2.1
+// @version      0.2.2
 // @description  自动刷新结果页
 // @license      GPL-3.0-or-later
 // @supportURL   https://github.com/YDX-2147483647/BIT-enhanced/issues
@@ -15,9 +15,11 @@
   function auto_refresh () {
     const titles = document.querySelectorAll('[role=main] > h3')
     if (titles[0].innerText === '查看程序的测试结果' &&
-            titles[0].nextSibling.innerText === '找不到您的程序') { return }
+      titles[0].nextSibling.innerText === '找不到您的程序') {
+      return
+    }
     if (titles[titles.length - 1].innerText !== '测试结果') {
-      location.reload()
+      window.location.reload()
     }
   }
   window.setInterval(auto_refresh, 2000)
