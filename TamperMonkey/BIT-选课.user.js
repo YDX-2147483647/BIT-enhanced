c6e75c9f4f9788453a08e2b5f955c3cf1d811a23
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2023-08-23 13:49:34 +0800

    chore: xk.bit.edu.cn 校内有 HTTPS

--- TamperMonkey/BIT-选课.user.js
+++ TamperMonkey/BIT-选课.user.js
@@ -7,6 +7,7 @@
 // @supportURL   https://github.com/YDX-2147483647/BIT-enhanced/issues
 // @author       Y.D.X.
 // @match        http://xk.bit.edu.cn/xsxkapp/sys/xsxkapp/*default/curriculavariable.do*
+// @match        https://xk.bit.edu.cn/xsxkapp/sys/xsxkapp/*default/curriculavariable.do*
 // @match        https://webvpn.bit.edu.cn/http/77726476706e69737468656265737421e8fc0f9e2e2426557a1dc7af96/xsxkapp/*
 // @grant        none
 // ==/UserScript==
